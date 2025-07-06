4a3ff7f766a2c7803032ad626d33f814d05ccc0e
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-06 21:52:02 +0100

    add some row in the table og comment

--- api/models/Comment.cs
+++ api/models/Comment.cs
@@ -8,6 +8,10 @@ namespace api.models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
         // foreign key
         public int? StockId { get; set; }
 
