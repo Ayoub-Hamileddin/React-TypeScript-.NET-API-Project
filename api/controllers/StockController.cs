61db73c3fd6b5c3d5ff872a7a7121da62c04b3eb
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-07 12:19:40 +0100

    ✨ Add CreateStock endpoint using DTOs and AutoMapper

--- api/controllers/StockController.cs
+++ api/controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.controllers
@@ -20,7 +22,7 @@ namespace api.controllers
         [HttpGet]
         public IActionResult GetAll() //IActionResult  it's a type of result like 404 NotFound()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(c => c.ToStockDto());
             return Ok(stocks);
         }
         [HttpGet("{id}")]
@@ -32,7 +34,30 @@ namespace api.controllers
                 return NotFound();
 
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto StockDto)
+        {
+            var stockModel = StockDto.ToStockFromCreateDto();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel }, stockModel.ToStockDto());
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null) return NotFound();
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
