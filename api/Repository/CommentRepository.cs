0c1a3990c122b8a5118189c6f7134f493fd42972
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-10 13:40:24 +0100

    🏗️ Implement create, update, and delete logic in CommentRepository

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -16,6 +16,23 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Comment> CreateAsync(Comment stockModel)
+        {
+            await _context.Comments.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Comment?> DeleteAsync(int commentId)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(c => c.Id == commentId);
+            if (commentModel == null) return null;
+            _context.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllCommentAsync()
         {
             return await _context.Comments.ToListAsync();
@@ -24,6 +41,16 @@ namespace api.Repository
         public async Task<Comment?> GetByIdAsync(int id)
         {
             return await _context.Comments.FindAsync(id);
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment comment)
+        {
+            var excitingComment = await _context.Comments.FindAsync(id);
+            if (excitingComment == null) return null;
+            excitingComment.Content = comment.Content;
+            excitingComment.Title = comment.Title;
+            await _context.SaveChangesAsync();
+            return comment;
         }
     }
 }
