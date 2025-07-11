06ce911b707b4b1d1a8f535f11c21b7a9d06d340
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-11 22:13:29 +0100

    ✅ Add validation rules to CreateCommentDto

--- api/Dtos/Comment/CreateCommentDto.cs
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required(ErrorMessage="le text du text est obligatoire")]
+        [MinLength(5,ErrorMessage="le minumum est 5 lettre")]
+        [MaxLength(200,ErrorMessage="le maximum est 5 lettre")]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage="le text du text est obligatoire")]
+        [MinLength(5,ErrorMessage="le minumum est 5 lettre")]
+        [MaxLength(200,ErrorMessage="le maximum est 5 lettre")]
         public string Content { get; set; } = string.Empty;
     }
 }
